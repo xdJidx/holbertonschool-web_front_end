7252f90d941ed232b41d290f160ac527bf593459
xdJidx <bernardjerome59@gmail.com>
2024-02-02 10:03:53 +0100

    Simple callback

---
+++ Javascript_advanced/14-wikipedia.js
@@ -0,0 +1,29 @@
+function createElement(data) {
+    const paragraph = document.createElement('p');
+    paragraph.textContent = data;
+    document.body.appendChild(paragraph);
+  }
+  
+  function queryWikipedia(callback) {
+    const xhr = new XMLHttpRequest();
+    const url = 'https://en.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&exintro&explaintext&redirects=1&titles=Stack%20Overflow&origin=*';
+  
+    xhr.open('GET', url, true);
+  
+    xhr.onreadystatechange = function () {
+      if (xhr.readyState === 4 && xhr.status === 200) {
+        const response = JSON.parse(xhr.responseText);
+        const page = Object.values(response.query.pages)[0];
+        const extract = page.extract;
+  
+        // Call the callback function with the extract
+        callback(extract);
+      }
+    };
+  
+    xhr.send();
+  }
+  
+  // Call queryWikipedia with createElement as the callback
+  queryWikipedia(createElement);
+  
